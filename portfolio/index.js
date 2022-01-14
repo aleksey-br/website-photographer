cc9ef07aafcea4873a2c51f62d0bb8512c5c5563
Alex Br <maskall1993@gmail.com>
2022-01-14 17:34:22 +0300

    fix: corrected section

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,15 +1,3 @@
-const langBtn = document.querySelectorAll(".lang__item");
-
-langBtn.forEach((item) => {
-  item.addEventListener("change", (e) => {
-    langBtn.forEach((el) => {
-      el.classList.remove("active");
-    });
-
-    item.classList.add("active");
-  });
-});
-
 //self-esteem
 
 console.log(`
